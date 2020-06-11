c72508debd0857f78b7633104d1db8bb4b05893e
Justin Hamilton <JHamilton@Pas.com>
2020-06-11 10:32:42 -0500

    Add project files.

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AnimalsMvc.Models;
+
+namespace AnimalsMvc.Controllers
+{
+    public class AnimalsController : Controller
+    {
+        private IData _tempData;
+
+        public AnimalsController(IData tempData)
+        {
+            _tempData = tempData;
+        }
+        public IActionResult Index()
+        {
+            var animals = _tempData.AnimalsInitializeData();
+            var indexViewModel = new IndexViewModel();
+            indexViewModel.Animals = animals;
+            return View(indexViewModel);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            var model = _tempData.GetAnimalById(id);
+            if (model == null)
+                return NotFound();
+            return View(model);
+        }
+    }
+}
---
+++ Models/IData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalsMvc.Models
+{
+    public interface IData
+    {
+        public List<Animal> AnimalsList { get; set; }
+        public List<Animal> AnimalsInitializeData();
+        public Animal GetAnimalById(int? id);
+    }
+}
